aeed8830945dc82808712227e1f6228c40636519
PrinceStarLord <86295965+PrinceStarLord@users.noreply.github.com>
2021-06-22 16:51:09 +0530

    Update channel.py

--- bot/plugins/channel.py
+++ bot/plugins/channel.py
@@ -190,7 +190,7 @@ async def connect(bot: Bot, update):
     await db.add_chat(chat_id, channel_id, channel_name)
     await recacher(chat_id, True, True, bot, update)
     
-    await wait_msg.edit_text(f"Channel Was Sucessfully Added With <code>{len(data)}</code> Files..")
+    await wait_msg.edit_text(f"Channel Was Sucessfully Added 😀 With <code>{len(data)}</code> Files..")
 
 
 @Client.on_message(filters.command(["del"]) & filters.group, group=1)
